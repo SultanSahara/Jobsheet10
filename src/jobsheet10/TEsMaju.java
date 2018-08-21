498daef3defafbe7666efaba822cf7f65c68c10e
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-21 15:12:53 +0700

    Initial Commit

---
+++ src/jobsheet10/TEsMaju.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet10;
+
+/**
+ *
+ * @author ASUS
+ */
+public class TEsMaju {
+    public static void main (String[]args){
+        int kelas [][]= {{10},{11},{12}};
+        String Jurusan [][] = {{"RPL"},{" TKJ"}};
+	System.out.println(kelas [0][0]+Jurusan[0][0]);
+	}
+}
+
+	
+	
+    
+    
+
